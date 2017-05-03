00bf4cb0ea1611894c0a2bee58f0634f4a1ac62f
Greattony Oduche <greattony96@gmail.com>
2017-05-04 01:20:38 +0300

    Styling, Staticfiles and Crispyform

--- src/koura/settings.py
+++ src/koura/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'question',
+    'crispy_forms',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +102,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_root")
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_root")
+
+
+# crispy forms
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
